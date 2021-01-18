fc6f18e2e0ed56d346270105394ac17ba74f120e
Egor <evn88fx64@gmail.com>
2021-01-18 22:04:45 +0300

    add data props

--- react-front/src/components/Export.jsx
+++ react-front/src/components/Export.jsx
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export function Export({name="export"}) {
+export function Export({name="export", data=[]}) {
   return <button className="btn btn-default">{name}</button>
 }
